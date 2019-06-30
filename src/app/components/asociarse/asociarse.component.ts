e95094ff8d0e350075e7dfae2095e95ded0b89d9
Matías Iturri <iturrima@gmail.com>
2019-06-30 20:42:33 -0300

    form asociarse ok

--- src/app/components/asociarse/asociarse.component.ts
+++ src/app/components/asociarse/asociarse.component.ts
@@ -1,11 +1,34 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SendmailService } from '../../services/sendmail.service';
+import { Router } from '@angular/router';
+
 
 @Component({
 selector: 'app-asociarse',
-templateUrl:'./asociarse.component.html'
+templateUrl:'./asociarse.component.html',
+styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+    `]
 
 })
 
 export class AsociarseComponent{
+
+    constructor(private mailService: SendmailService,
+        private router: Router) {}
+
+enviar(forma: NgForm) {
+
+if (forma.value.name && forma.value.email && forma.value.message && forma.value.subject) {
+this.mailService.sendMail(forma.value)
+    .subscribe( resp => {
+      console.log( resp );
+    });
+this.router.navigate(['../gracias']);
+}
+}
 
 }
